59be811db3b4e4a819423e9b9412a9b683d2e799
hoping <stevenfok@126.com>
2020-06-05 22:48:10 +0800

    a new file .cpp

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+ ///
+ /// @file    hello.cpp
+ /// @author  hoping(stevenfok@126.com)
+ /// @date    2020-06-05 22:46:54
+ ///
+ 
+#include <iostream>
+using std::cout;
+using std::endl;
+ 
+int main(void)
+{
+	int ;
+ 
+}
